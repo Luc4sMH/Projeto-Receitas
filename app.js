92d6c8e5fac9ddce4f15319c195404685d361c93
Lucas M. Honorato <lucasmenhon@gmail.com>
2024-09-06 22:42:28 -0300

    subindo informação correta
    Subindo tudo corretamente

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 function pesquisar() {
-    // Seleciona a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-  
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
-    let resultados = "";
-  
-    // Itera sobre cada resultado da pesquisa
-    for (let dado of dados) {
-      // Cria o HTML para cada resultado, incluindo título, descrição, panela e links
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="${dado.link}" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <p>Prato feito na: ${dado.panela}</p>
-          <a href="${dado.link}" target="_blank">Saiba mais sobre o prato</a>
-        </div>
-      `;
-    }
-  
-    // Atribui o HTML gerado à seção de resultados
-    section.innerHTML = resultados;
+ 
+  let section = document.getElementById("resultados-pesquisa");
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  if (!campoPesquisa) {
+      section.innerHTML = "Digite algo para buscar..."
+      return
   }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let link = "";
+  let panela = "";
+  let tags = "";
+
+  for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    link = dado.link.toLowerCase()
+    panela = dado.panela.toLowerCase()
+    tags = dado.tags.toLowerCase()
+  
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || link.includes(campoPesquisa) || panela.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      resultados += `
+      <div class="item-resultado">
+        <h2>
+          <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <p>Prato feito na: ${dado.panela}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais sobre o prato</a>
+      </div>
+    `;
+    }
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nenhuma receita encontrada</p>"
+  }
+
+  section.innerHTML = resultados;
+}
